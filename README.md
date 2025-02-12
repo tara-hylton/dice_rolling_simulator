7e98ed8d85c0320fe92dd49579716ba26910a92b
tara-hylton <turrrleigh@gmail.com>
2025-02-12 17:20:46 -0500

    added a while loop so you can roll multiple dice of same type at once, updated readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 <!-- dice_rolling_simulator-->
-#Dice rolling simulator
-##This sim can roll a 4, 6, 8, 10, 12 or 20 sided die 
+Dice rolling simulator  
+This sim can roll a 4, 6, 8, 10, 12 or 20 sided die 
 <!--This code uses match case to decide what dice is needed along with user input and the import random feature-->
-###Added a loop so you can continue to roll dice or quit
+Added a loop so you can continue to roll dice or quit  
+Added a loop so you can roll multiple of the same dice at one time
+<!--This did result in an error in the replay loop which I will work on in my next session-->
 
 
--- dice_rolling_sim.py
+++ dice_rolling_sim.py
@@ -1,29 +1,41 @@
 import random
 
-def roll_dice(sides):
 
-    match sides:
-        case "4":
-            print(random.randint(1, 4))
-        case "6":
-            print(random.randint(1, 6))
-        case "8":
-            print(random.randint(1, 8))
-        case "10":
-            print(random.randint(1, 10))
-        case "12":
-            print(random.randint(1, 12))
-        case "20":
-            print(random.randint(1, 20))
-        case _:
-            print("Not a valid choice, roll again")
-print("What die would you like to roll?\n 4 sided\n 6 sided\n 8 sided\n 10 sided\n 12 sided\n 20 sided")
+
+def roll_dice(sides):
+    i = 0
+    total_roll = 0
+    while i < number_of_dice:
+        i += 1
+        match sides:
+            case "4":
+                total_roll += (random.randint(1, 4))
+            case "6":
+                total_roll += (random.randint(1, 6))
+                print(total_roll, i)
+            case "8":
+                print(random.randint(1, 8))
+            case "10":
+                print(random.randint(1, 10))
+            case "12":
+                print(random.randint(1, 12))
+            case "20":
+                print(random.randint(1, 20))
+            case _:
+                print("Not a valid choice, roll again")
+
+    print(total_roll, i)
+
+print("What die would you like to roll?\n 4 sided\n 6 sided\n 8 sided\n 10 sided\n 12 sided\n 20 sided\n")
 sides = input()
+print("How many dice are you rolling?")
+number_of_dice = int(input())
 roll_dice(sides)
 while True:
     answer = input("Would you like to roll again? y/n\n").lower()
     if answer in ("y"):
-        print("What die would you like to roll?\n 4 sided\n 6 sided\n 8 sided\n 10 sided\n 12 sided\n 20 sided")
+        print("What die would you like to roll?\n 4 sided\n 6 sided\n 8 sided\n 10 sided\n 12 sided\n 20 sided\n")
+        number_of_dice = input("How many dice are you rolling?\n")
         sides = input()
         roll_dice(sides)
     elif answer in ("n"):
