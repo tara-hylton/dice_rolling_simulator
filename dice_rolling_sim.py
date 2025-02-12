c01119ea78fad139906011197a34c69e73c021f5
tara-hylton <turrrleigh@gmail.com>
2025-02-12 15:49:03 -0500

    added loop to include rolling again, stopping

--- dice_rolling_sim.py
+++ dice_rolling_sim.py
@@ -1,6 +1,7 @@
 import random
 
 def roll_dice(sides):
+
     match sides:
         case "4":
             print(random.randint(1, 4))
@@ -16,10 +17,20 @@ def roll_dice(sides):
             print(random.randint(1, 20))
         case _:
             print("Not a valid choice, roll again")
-
 print("What die would you like to roll?\n 4 sided\n 6 sided\n 8 sided\n 10 sided\n 12 sided\n 20 sided")
 sides = input()
 roll_dice(sides)
+while True:
+    answer = input("Would you like to roll again? y/n\n").lower()
+    if answer in ("y"):
+        print("What die would you like to roll?\n 4 sided\n 6 sided\n 8 sided\n 10 sided\n 12 sided\n 20 sided")
+        sides = input()
+        roll_dice(sides)
+    elif answer in ("n"):
+        print("May all of your rolls be critical. Goodbye.")
+        break
+    else:
+        print("Not a valid choice. Would you like to roll again?")
 
 
 
